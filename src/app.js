646b587f4587e24062b0bc78d9c20e5c9a6da9b1
Harshit Sharma <harshitsharma06@gmail.com>
2025-04-26 17:43:39 +0530

    exploring Postman

--- src/app.js
+++ src/app.js
@@ -2,13 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the test");
+// app.use("/", (req, res) => {
+//     res.send("hello from server");
+// })
+
+
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the test");
+// });
+
+
+app.get("/user", (req, res) =>{
+    res.send({first_name: 'Harshit', Last_name: "Sharma"});
+
 });
 
-app.use((req, res) => {
-    res.send("hello from server");
-})
+app.post("/user", (req, res) =>{
+    res.send("data successfully saved in the database");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("data deleted successfully");
+});
+
 
 app.listen(7777, () => {
     console.log("Server is Listening port 7777.....");
